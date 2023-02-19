6a9bf7dbab37f71bb53291b10866954bd1f9ca60
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-02-19 11:22:06 +0200

    add cors middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import mongoose from "mongoose";
+import cors  from "cors";
 import { blogsController } from "./controller/blogsController.js";
 import { authController } from "./controller/authController.js";
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 mongoose.connect(process.env.DB_URL, () => {
   console.log("connected to db");
 });
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/blogs", blogsController);
 app.use("/api/v1/auth", authController);
