9e7e29bad037a22fcf78eb8ba433c0a7cb92a8a9
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-06-14 09:46:54 +0300

    add pnpm as package manager add jsdoc

--- middleware/secureRoute.js
+++ middleware/secureRoute.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
 import { User } from "../db/userSchema.js";
+import { vars } from "../config/vars.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const bearerToken = req.headers.authorization;
     if (!bearerToken?.startsWith("Bearer"))
       return res.status(401).json({ message: "loggin failed" });
-    if (jwt.verify(bearerToken.split(" ")[1], process.env.JWT_SECRET)) {
+    if (jwt.verify(bearerToken.split(" ")[1], vars.JWT_SECRET)) {
       const decodedData = jwt.decode(bearerToken.split(" ")[1]);
       const user = await User.findById(decodedData.id);
       req.user = user;
