01ecd870bd99ca65aacc53f5f642b06f86bcc0eb
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-06-14 10:58:38 +0300

    fix tsconfig rules

--- controller/authController.js
+++ controller/authController.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcrypt";
 import Jwt from "jsonwebtoken";
 import { User } from "../db/userSchema.js";
+import { vars } from "../config/vars.js";
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
@@ -23,10 +24,10 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) res.status(401).json({ message: "user account not found" });
-    console.log(user);
+    if (!user) return res.status(401).json({ message: "user account not found" });
+
     if (bcrypt.compareSync(password, user.password)) {
-      const token = Jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      const token = Jwt.sign({ id: user.id }, vars.JWT_SECRET, {
         expiresIn: "2d",
       });
       return res.send({ token });
