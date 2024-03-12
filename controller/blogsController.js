41169f188c4794e5f979a6c34a4df57584625e54
musllim <muslimuwitondanishema@gmail.com>
2024-03-12 20:33:06 +0200

    validate req.body

--- controller/blogsController.js
+++ controller/blogsController.js
@@ -1,49 +1,94 @@
-import express from "express";
+import Joi from 'joi'
 import { Blog } from "../db/blogSchema.js";
-import { protectRoute } from "../middleware/secureRoute.js";
-import { vars } from "../config/vars.js";
-const router = express.Router();
 
-router
-  .route("/")
-  .get(async (req, res) => {
-    try {
-      const blogs = await Blog.find();
-      res.send(blogs);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+/**
+ * 
+ * @param {import("express").Response} res 
+ * @returns 
+ */
+
+export async function getAllBlogsController(_, res) {
+  try {
+    const blogs = await Blog.find();
+    res.send(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @returns 
+ */
+
+export async function createBlogController(req, res) {
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    image: Joi.string().required(),
+    body: Joi.string().required(),
+    subTitle: Joi.string()
   })
-  .post(protectRoute, async (req, res) => {
-    if (!(req.user.email === vars.ADMIN_EMAIL))
+
+  const { error, value, warning } = schema.validate(req.body)
+  if (error)
+    return res.status(400).json(error)
+  if (warning) res.status(400).json(warning)
+  try {
+    if (!(req.user.email === process.env.ADMIN_EMAIL))
       return res.status(401).json({ message: "only admin can post a blog" });
     const { title, body, image, subTitle } = req.body;
-    const blog = await Blog.create({ title, body, image, subTitle });
-    res.send(blog);
-  });
+    const blog = await Blog.create(value);
+    res.json({ data: blog, warning, message: 'created successfuly' });
 
-router
-  .route("/:id")
-  .get(async (req, res) => {
-    const blog = await Blog.findById(req.params.id);
-    res.send(blog);
-  })
-  .put(protectRoute, async (req, res) => {
-    if (!(req.user.email === vars.ADMIN_EMAIL))
-      return res.status(401).json({ message: "only admin can update a blog" });
-    const { title, body, subTitle, image } = req.body;
-    const updates = {};
-    if (title) updates.title = title;
-    if (body) updates.body = body;
-    if (subTitle) updates.subTitle = subTitle;
-    if (image) updates.image = image;
-    const blog = await Blog.findByIdAndUpdate(req.params.id, updates);
-    res.send(blog);
-  })
-  .delete(protectRoute, async (req, res) => {
-    if (!(req.user.email === vars.ADMIN_EMAIL))
-      return res.status(401).json({ message: "only admin can delete a blog" });
-    const blog = await Blog.findByIdAndDelete(req.params.id);
-    res.send(blog);
-  });
-export { router as blogsController };
+  } catch (error) {
+    res.status(500).json(error)
+  }
+
+}
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @returns 
+ */
+
+export async function getBlogController(req, res) {
+  const blog = await Blog.findById(req.params.id);
+  res.send(blog);
+}
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @returns 
+ */
+export async function deleteBlogController(req, res) {
+  if (!(req.user.email === process.env.ADMIN_EMAIL))
+    return res.status(401).json({ message: "only admin can delete a blog" });
+  const blog = await Blog.findByIdAndDelete(req.params.id);
+  res.send(blog);
+}
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @returns 
+ */
+export async function updateBlogController(req, res) {
+  if (!(req.user.email === process.env.ADMIN_EMAIL))
+    return res.status(401).json({ message: "only admin can update a blog" });
+  const { title, body, subTitle, image } = req.body;
+  const updates = {};
+  if (title) updates.title = title;
+  if (body) updates.body = body;
+  if (subTitle) updates.subTitle = subTitle;
+  if (image) updates.image = image;
+  const blog = await Blog.findByIdAndUpdate(req.params.id, updates);
+  res.send(blog);
+}
+
